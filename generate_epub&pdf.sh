9d6a907670a6565a9edf9a5600428f8f705719a2
wtsnwei <delphizt@foxmail.com>
2021-07-20 16:36:51 +0800

    添加生成离线文件脚本

---
+++ generate_epub&pdf.sh
@@ -0,0 +1,12 @@
+# 合并文件
+mkdocscombine -o mydocs.pd
+
+# 处理中间文件
+sed -i 's/{: .page-title}//g' mydocs.pd
+
+# 生成epub
+pandoc --toc -f markdown+grid_tables -t epub -o mydocs.epub mydocs.pd -V mainfont=".PingFang SC"
+
+# 生成pdf
+# mainfont为操作系统上存在的字体
+pandoc mydocs.pd -o mydocs.pdf --latex-engine=xelatex -V mainfont=".PingFang SC" --template=template.tex
