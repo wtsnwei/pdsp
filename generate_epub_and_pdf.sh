c776d97853b265467c4ec70db91b2a55b3371f0a
wtsnwei <delphizt@foxmail.com>
2021-08-11 15:34:34 +0800

    update

--- generate_epub_and_pdf.sh
+++ generate_epub_and_pdf.sh
@@ -5,14 +5,14 @@ mkdocscombine -o pdsp.pd
 sed -i 's/{: .page-title}//g' pdsp.pd
 
 # 生成epub
-pandoc --toc -f markdown+grid_tables -t epub -o pdsp.epub pdsp.pd -V mainfont=".PingFang SC"
+pandoc --toc -f markdown+grid_tables -t epub -o pdsp.epub pdsp.pd
 
 # 生成pdf, 推荐使用 eisvogel 模板
 pandoc "pdsp.pd" -o "pdsp.pdf" --from markdown --template "eisvogel.tex" --listings --latex-engine "xelatex" -V CJKmainfont=".PingFang SC"
 
 
 # mainfont为操作系统上存在的字体
-# pandoc pdsp.pd -o pdsp.pdf --latex-engine=xelatex -V mainfont=".PingFang SC" --template=template.tex
+# pandoc pdsp.pd -o pdsp.pdf --latex-engine=xelatex -V CJKmainfont=".PingFang SC" --template=template.tex
 
 # 删除生成的中间文件
 rm *.pd
